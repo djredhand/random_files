ea9ca6a2801e73ce0c23f488e9349beb10926211
djredhand <djredhand@gmail.com>
2013-10-05 15:49:13 -0400

    jQuery functionized

--- enservio/show_hide.js
+++ enservio/show_hide.js
@@ -1,26 +1,32 @@
-$(document).ready(function(){
 
-	function listen_handle_close_div(div){
-		div = $(div);
-		div.click(function(){
-			$(this).parent().slideToggle();
-		})
-		return div
-	}
+(function($){
+	$.fn.enservio_toggle= function(){ 
+		function listen_handle_close_div(div){
+			div = $(div);
+			div.click(function(){
+				$(this).parent().slideToggle();
+			})
+			return div
+		}
 
-    $('p.show_hide_span').click(function(){
-    	var slide_div = $(this).next('.slidingDiv');
-    	var close_div = $('<div class="close_slide_div"><p class="show_hide_span">Hide</p></div>');
-    	slide_div.addClass('opened_slide_div');
-    	slide_div.slideToggle();
-    	
+	    $('p.show_hide_span').click(function(){
+	    	var slide_div = $(this).next('.slidingDiv');
+	    	var close_div = $('<div class="close_slide_div"><p class="show_hide_span">Hide</p></div>');
+	    	slide_div.addClass('opened_slide_div');
+	    	slide_div.slideToggle();
+	    	
 
-    	if(!$(this).next().children('.close_slide_div').length >0){
-    		slide_div.append(close_div);
-    		listen_handle_close_div(close_div);
-    	}
-    });
+	    	if(!$(this).next().children('.close_slide_div').length >0){
+	    		slide_div.append(close_div);
+	    		listen_handle_close_div(close_div);
+	    	}
+	    });
 
-    $(".slidingDiv").hide();
+	    $(".slidingDiv").hide();
+	};
+})(jQuery);
 
-});
+
+jQuery(document).ready(function(){
+	$.fn.enservio_toggle()
+})
