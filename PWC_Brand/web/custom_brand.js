632d75e01e24f38d6963b5c3dd847326bdc7d758
djredhand <djredhand@gmail.com>
2013-12-03 00:47:51 -0500

    almost working tabs

--- PWC_Brand/web/custom_brand.js
+++ PWC_Brand/web/custom_brand.js
@@ -2,6 +2,7 @@
 var custom_brand = {
 
 	init: function(){
+		custom_brand.tab_nav();
 		custom_brand.column_height();
 	},
 
@@ -13,6 +14,22 @@ var custom_brand = {
 			height: col_height + 'px'
 		})
 		window.col = col; 
+	},
+
+	tab_nav: function(){
+		jQuery('.tab-content-wrap:eq(0)').hide();//temporary
+		jQuery('.tab-content .tab-nav li').click(function(){
+			var items = jQuery('.tab-content .tab-nav li');
+			var index = items.index(jQuery(this));
+			custom_brand.show_tab(index)
+		})
+	},
+
+	show_tab: function(index){
+		console.log(index);
+		jQuery('.tab-content-wrap').hide()
+		var tab = jQuery(jQuery('.tab-content-wrap')[index]);
+		tab.show();
 	}
 }
 
