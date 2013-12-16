c286d1e9f6fc07fb642a0dfe3904d956bdf20864
emmett <emmett@127.0.0.1 www.localhost.com ubuntu>
2013-12-15 19:52:59 -0500

    working toggle

--- PWC_Brand/web/custom_brand.js
+++ PWC_Brand/web/custom_brand.js
@@ -3,17 +3,26 @@ var custom_brand = {
 
 	init: function(){
 		custom_brand.tab_nav();
+		
+		custom_brand.brand_toggle('.brand-toggle');
 		custom_brand.column_height();
 	},
 
 	column_height: function(){
 		
 		var col = jQuery('.col.brand-custom');
-		var col_height = col.parent().height() -12;
-		col.css({
-			height: col_height + 'px'
+		col.each(function(){
+			var c = jQuery(this);
+			c.css('height', '100%');//reset the height 
+			var col_height = c.parent().height() -12;
+			c.css({
+				height: col_height + 'px'
+			})
+			window.col = c;
 		})
-		window.col = col; 
+		
+		
+		 
 	},
 
 	tab_nav: function(){
@@ -30,6 +39,42 @@ var custom_brand = {
 		jQuery('.tab-content-wrap').hide()
 		var tab = jQuery(jQuery('.tab-content-wrap')[index]);
 		tab.show();
+	},
+
+	brand_toggle:function(el){
+		el = jQuery(el);
+		el.each(function(){
+			var header = jQuery(this).children('.brand-toggle-header');
+			var toggle_symbol = jQuery('<div class="toggle-symbol"></div>');
+			var toggle_content = jQuery(this).children('.brand-toggle-content');
+			
+			var clicker = function(header, toggle_symbol){
+				header.click(function(){
+					var content = header.parent().children('.brand-toggle-content');
+					console.log(toggle_symbol.hasClass('plus'));
+					if(toggle_symbol.hasClass('plus')){
+						toggle_symbol.removeClass('plus').addClass('mdash');
+						content.fadeIn(500)
+					}else{
+						toggle_symbol.removeClass('mdash').addClass('plus');
+						content.hide();
+					}
+					custom_brand.column_height()
+				});
+
+			}
+
+			header.prepend(toggle_symbol);
+			toggle_symbol.addClass('plus');
+			toggle_content.hide();
+			clicker(header, toggle_symbol);
+
+		})
+
+
+
+		console.log(el);
+
 	}
 }
 
