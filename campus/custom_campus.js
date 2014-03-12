826014731b1bf5b8dc4e2eb792e412e370d002e3
djredhand <djredhand@gmail.com>
2014-03-12 02:43:59 -0400

    changes

--- campus/custom_campus.js
+++ campus/custom_campus.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
+// the career page
 	jQuery('.program-row').hover(function(){
 		var grid_items = jQuery(this).children('.grid');
 		var program_content = jQuery(this).children('.program-content');
@@ -19,13 +20,30 @@ jQuery(document).ready(function(){
 		program_content.hide();
 		grid_items.show();
 	})
-	function hexc(colorval) {
-	    var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-	    delete(parts[0]);
-	    for (var i = 1; i <= 3; ++i) {
-	        parts[i] = parseInt(parts[i]).toString(16);
-	        if (parts[i].length == 1) parts[i] = '0' + parts[i];
-	    }
-	    color = '#' + parts.join('');
-	}
+//tax page 
+	jQuery('.tax-toggle').hover(function(){
+		var inner_content = jQuery(this).children('.inner-content');
+		var chev = jQuery(this).find('.chevron');
+		var height_adj = inner_content.height();
+		var column = jQuery('#career-path');
+		window.column_height = column.height();
+		chev.css({ WebkitTransform: 'rotate(' + 90 + 'deg)'});
+      // For Mozilla browser: e.g. Firefox
+        chev.css({ '-moz-transform': 'rotate(' + 90 + 'deg)'});
+		column.height(column_height + height_adj);
+		inner_content.show();
+		console.log(inner_content.text())
+	},function(){
+		var chev = jQuery(this).find('.chevron');
+		var inner_content = jQuery(this).children('.inner-content');
+		var column = jQuery('#career-path');
+		column.height(window.column_height);
+		chev.css({ WebkitTransform: 'rotate(' + 0 + 'deg)'});
+      // For Mozilla browser: e.g. Firefox
+        chev.css({ '-moz-transform': 'rotate(' + 0 + 'deg)'});
+		inner_content.hide();
+
+	})
+
+
 })
